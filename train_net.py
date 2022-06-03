5ee730f277c9da42edfc248764bb84a63df73fa1
me <dave@dave.com>
2022-06-03 08:48:07 +0000

    replaced train net

--- train_net.py
+++ train_net.py
@@ -1,5 +1,5 @@
 """
-Title:      train_net.py
+Title:      train_network.py
 
 Fuction:    This script acts as the top level script for training all models for potato tuber
             segmentation. The script will configure the pipeline to the required parameters for
@@ -8,9 +8,9 @@ Fuction:    This script acts as the top level script for training all models for
 
 Edited by:  Bradley Hurst 
 """
-# ============================
+# =================================================================================================
 # Importing packages/libraries 
-# ============================
+# =================================================================================================
 # pytorch imports
 import json
 
@@ -33,244 +33,216 @@ from utils import model_saver, make_dir, time_converter, set_seed, data_loader_m
 from coco_evaluation import evaluate
 import configs
 from plotter import plot_lr_loss, plot_precision_recall, plot_f1_score, plot_cent_err
-# ============================
+# =================================================================================================
 # Train_net implementation
-# ============================
-def main(config_dict, seed=42):
+# =================================================================================================
+class TrainNetwork:
     """
-    Title:      Main
-
-    Function:   Main function carries out top level execution of all elements in the 
-                training process
-
-    Inputs:     - A dictionary of config parameters that is passed through the pipeline
-
-    Outputs:    - The most recent model saved in a results location
-                - Evaluation results from the model
-                - A copy of the configuration dictionary used to produce the model
-
-    Deps:       - COCOLoader in dataloader.py
-                - collate_function in dataloader.py
-                - model_selector in models.py
-                - opimizer_selector in optimizer.py
-                - lr_schedule_selector in optimizer.py
-                - train_one_epoch in engine.py
-                - val_one_epoch in engine.py
-                - evaluate in engine.py
-                - loop_saver from utils.py
-
-    Edited by:  Bradley Hurst 
+    class detials
     """
-    # ===== fixing random seed for reporducability ============================
-    # =========================================================================
-    # configuring device for cpu or gpu if available
-    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    def __init__(self, conf_dict, seed=42):
+        """
+        details
+        """
+        # collecting  configuration attributes
+        self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+        self.seed = seed
+        self.model = model_selector(conf_dict['model'], conf_dict['num_classes'], 
+                                    conf_dict['min_max']) 
+        self.params = [p for p in self.model.parameters() if p.requires_grad]
+        self.optimizer = optimizer_selector(conf_dict['optimizer'],
+                                   self.params,
+                                   conf_dict['optimizer_params'])
+        self.start_epoch = 0
+        self.out_dir = conf_dict['out_dir']
+        self.train = conf_dict['TRAIN']
+        self.test = conf_dict['TEST']
+        self.num_epochs = conf_dict['num_epochs']
+        self.print_freq = conf_dict['print_freq']
+        self.loss_dict = {}
+        self.plot_title = conf_dict['plot_title']
+        self.test_path = conf_dict['im_test_path']
 
-    # fixing the random seed for reproducability
-    set_seed(seed)
+        # calling attribute assigning methods  
+        self.schedule_assigner(conf_dict)
+        self.transforms_assigner(conf_dict)
+        self.loader_assigner(conf_dict)
+        self.optimizer_load(conf_dict)
+        self.config_saver(conf_dict)
+        
+        # training loop initialisiation
+        self.data_logging_init()
+        self.main()
     
-    # ===== handling data laodinging ==========================================
-    # =========================================================================
-    # configuring transforms for data loading
-    if config_dict['transforms'] != "":
-        transforms = transform_selector(config_dict['transforms'])
-    else:
-        transforms = None
-    
-    # collecting loaders
-    train_loader, validate_loader, test_loader = data_loader_manager(config_dict, seed, transforms)
+    # config methods
+    def schedule_assigner(self, conf_dict):
+        if conf_dict['lr_scheduler'] != "":
+            self.scheduler = lr_scheduler_selector(conf_dict['lr_scheduler'])
+        else:
+            self.scheduler = None
 
-    # ====== configuring model and solver =====================================
-    # =========================================================================
-    # get reqired model and set it to device
-    model = model_selector(config_dict['model'], config_dict['num_classes'], 
-                           config_dict['min_max'])
-    model.to(device)
+    def transforms_assigner(self, conf_dict):
+        if conf_dict['transforms'] != "":
+            self.transforms = transform_selector(conf_dict['transforms'])
+        else:
+            self.transforms = None
 
-    # get optimizer
-    params = [p for p in model.parameters() if p.requires_grad]
+    def loader_assigner(self, conf_dict):
+        tr_load, v_load, te_load = data_loader_manager(conf_dict, self.seed, self.transforms)
+        self.train_loader = tr_load
+        self.val_loader = v_load
+        self.test_loader = te_load
     
-    #config_dict['model_params'] = params
-    optimizer = optimizer_selector(config_dict['optimizer'],
-                                   params,
-                                   config_dict['optimizer_params'])
+    def optimizer_load(self, conf_dict):
+        if conf_dict['load_flag']:
+            checkpoint = torch.load(conf_dict["load"])
+            self.model.load_state_dict(checkpoint["state_dict"])
+            self.optimizer.load_state_dict(checkpoint["optimizer"])
+            self.start_epoch = checkpoint["epoch"]
+            for g in self.optimizer.param_groups:
+                g['lr'] = conf_dict['optimizer_params']['lr']
     
-    # get learning rate scheduler
-    if config_dict['lr_scheduler'] != "":
-        lr_scheduler = lr_scheduler_selector(config_dict['lr_scheduler'])
+    def config_saver(self, conf_dict):
+        config_save = self.out_dir + "/model_configs.json"
+        make_dir(self.out_dir)
+        with open(config_save, 'w') as f:
+            json.dump(conf_dict, f)
     
-    # define starting epoch
-    start_epoch = 0
+    def data_logging_init(self):
+        if self.train: 
 
-    # laoding model if required
-    if config_dict['load_flag']:
-        checkpoint = torch.load(config_dict["load"])
-        model.load_state_dict(checkpoint["state_dict"])
-        optimizer.load_state_dict(checkpoint["optimizer"])
-        start_epoch = checkpoint["epoch"]
-        for g in optimizer.param_groups:
-            g['lr'] = config_dict['optimizer_params']['lr']
+            # training loss dict
+            self.train_loss = {
+                'total': [],
+                #'classifier': [],
+                #'box_reg': [],
+                #'mask': [],
+                #'objectness': [],
+                #'rpn_box_reg': []
+            }
+        
+            # validation loss dict
+            self.val_loss = {
+                'total': [],
+                #'classifier': [],
+                #'box_reg': [],
+                #'mask': [],
+                #'objectness': [],
+                #'rpn_box_reg': []
+            }
+        
+            # evaluation dicitionary
+            self.val_eval = {
+                'mAP': []
+            }
+        
+            self.best_model = {
+                'mAP_val': [],
+                'epoch': []
+            }
 
-    # ====== saving config_dict ===============================================
-    # =========================================================================
-    # save train params here: ys save the json here!
-    config_save = config_dict['out_dir'] + "/model_configs.json"
-    make_dir(config_dict['out_dir'])
-    with open(config_save, 'w') as f:
-        json.dump(config_dict, f)
+    def main(self):
+        """
+        details
+        """
+        # sending model to device
+        self.model.to(self.device)
+        
+        # executing training 
+        if self.train:
+            self.training_exe()
+
+        # executing testing
+        if self.test:
+            self.testing_exe()
     
-    # ===== entering training loops ===========================================
-    # =========================================================================
-    # training loop implementation
-    if config_dict['TRAIN']: 
-        # data trackers
+    def training_exe(self):
         iter_count = 0
-        best_val = 0  # based of mAP, can be zero 
+        best_val = 0
 
-        # training loss dict
-        train_loss = {
-            'total': [],
-            'classifier': [],
-            'box_reg': [],
-            'mask': [],
-            'objectness': [],
-            'rpn_box_reg': []
-        }
-        
-        # validation loss dict
-        val_loss = {
-            'total': [],
-            'classifier': [],
-            'box_reg': [],
-            'mask': [],
-            'objectness': [],
-            'rpn_box_reg': []
-        }
-        
-        # evaluation dicitionary
-        val_eval = {
-            'mAP': []
-        }
-        
-        best_model = {
-            'mAP_val': [],
-            'epoch': []
-        }
-        
-        # loop through epochs
-        for epoch in range(start_epoch, start_epoch + config_dict['num_epochs']):
-            
+        # loop through the epochs
+        for epoch in range(self.start_epoch, self.num_epochs):
             # begin epoch count
             epoch_begin = time.time()
-            
+
             # train one epoch
-            acc_train_loss, iter_count = train_one_epoch(train_loader, model, device, optimizer, 
-                                                        config_dict['print_freq'], iter_count, epoch)
+            acc_train_loss, iter_count = train_one_epoch(self.train_loader, self.model, self.device,
+                                                self.optimizer, self.print_freq, iter_count, epoch)
+            
+            # stepping scheduler if present
+            if self.scheduler != None:
+                self.scheduler.step()
+            
+            # validating one epoch
+            acc_val_loss = validate_one_epoch(self.val_loader, self.model, self.device)#
 
-            # if applicable, lr schedule step
-            if config_dict['lr_scheduler'] != "":
-                lr_scheduler.step()
-            
-            # validate one epoch
-            acc_val_loss = validate_one_epoch(validate_loader, model, device)
-            
-            # testing mAP
-            mAP_val = evaluate(model, validate_loader, device, config_dict['out_dir'],
-                 train_flag=config_dict['TRAIN'])
+            # validation eval
+            mAP_val = evaluate(self.model, self.val_loader, self.device, self.out_dir,
+                                train_flag=self.train)
 
-            # getting summariesed losses
-            train_summary = sum(acc_train_loss['total']) / len(acc_train_loss['total'])
-            validation_summary = sum(acc_val_loss['total']) / len(acc_val_loss['total'])
-            #print("Training summary: %s, Validation summary: %s" % (train_summary, validation_summary))
-            
-            # checking and saving model
+            # collecting data
+            self.train_loss['total'].append(sum(acc_train_loss['total']) / len(acc_train_loss['total']))
+            self.val_loss['total'].append(sum(acc_val_loss['total']) / len(acc_val_loss['total']))
+            self.val_eval['mAP'].append(mAP_val)
+
+            # saving models
             prev_best = best_val
-            best_val = model_saver(epoch, model, optimizer, best_val, mAP_val,
-                                    config_dict['out_dir'])
-            
-            # logging and saving results from training and validation
-            train_loss['total'].append(train_summary)
-            train_loss['classifier'].append(sum(acc_train_loss['classifier']) / len(acc_train_loss['classifier']))
-            train_loss['box_reg'].append(sum(acc_train_loss['box_reg']) / len(acc_train_loss['box_reg']))
-            train_loss['mask'].append(sum(acc_train_loss['mask']) / len(acc_train_loss['mask']))
-            train_loss['objectness'].append(sum(acc_train_loss['objectness']) / len(acc_train_loss['objectness']))
-            train_loss['rpn_box_reg'].append(sum(acc_train_loss['rpn_box_reg']) / len(acc_train_loss['rpn_box_reg']))
-            
-            val_loss['total'].append(validation_summary)
-            val_loss['classifier'].append(sum(acc_val_loss['classifier']) / len(acc_val_loss['classifier']))
-            val_loss['box_reg'].append(sum(acc_val_loss['box_reg']) / len(acc_val_loss['box_reg']))
-            val_loss['mask'].append(sum(acc_val_loss['mask']) / len(acc_val_loss['mask']))
-            val_loss['objectness'].append(sum(acc_val_loss['objectness']) / len(acc_val_loss['objectness']))
-            val_loss['rpn_box_reg'].append(sum(acc_val_loss['rpn_box_reg']) / len(acc_val_loss['rpn_box_reg']))
-            
-            val_eval['mAP'].append(mAP_val)
-            
+            best_val = model_saver(epoch, self.model, self.optimizer, best_val, mAP_val, self.out_dir)
             if best_val > prev_best:
-                best_model['mAP_val'].append(best_val)
-                best_model['epoch'].append(epoch+1)
-
-            delta = time.time() - epoch_begin
-            epoch_duration = time_converter(delta) 
-            print("Epoch Duration: ", epoch_duration)
+                self.best_model['mAP_val'].append(best_val)
+                self.best_model['epoch'].append(epoch+1)
             
+            # finish epoch count
+            delta = time.time() - epoch_begin
+            epoch_duration = time_converter(delta)
+            print("Epoch Duration: ", epoch_duration)
+
         # recording losses  
-        losses_dict = {
-            'train_loss': train_loss,
-            'val_loss': val_loss,
-            'val_eval': val_eval,
-            'best_val': best_model
+        self.loss_dict = {
+            'train_loss': self.train_loss,
+            'val_loss': self.val_loss,
+            'val_eval': self.val_eval,
+            'best_val': self.best_model
         }
         
-        loss_path = config_dict['out_dir'] + "/loss_results.json"
+        # saving data in json
+        loss_path = self.out_dir + "/loss_results.json"
         with open(loss_path, "w") as f:
-            json.dump(losses_dict, f)
-            
-    # ===== calling evaluation and plotting ===================================
-    # =========================================================================
-    # producing plots    
-    if config_dict['TRAIN']:
-        # path for loading json
-        losses_json = config_dict['out_dir'] + "/loss_results.json"
-        
-        # getting data from json
-        with open(losses_json) as losses_file:
-            loss_dict = json.load(losses_file)
-            losses_file.close()
-        
-        # producting lr loss plot
-        plot_lr_loss(loss_dict, config_dict['plot_title'], config_dict['out_dir'])
+            json.dump(self.loss_dict, f)
 
-    if config_dict['TEST']:
-        # model evaluation
-        evaluate(model, test_loader, device, config_dict['out_dir'])
-        centroid_evaluation(model, test_loader, device, config_dict['out_dir'])
-        
-        # defining model locations 
-        pr_json = config_dict['out_dir'] + "/precision_recall_results.json"
-        cen_err_json = config_dict['out_dir'] + "/centroid_error.json"
-        
-        # getting data from json
+        # plotting data
+        plot_lr_loss(self.loss_dict, self.plot_title, self.out_dir)
+
+    def testing_exe(self):
+        # carrying out evaluations
+        evaluate(self.model, self.test_loader, self.device, self.out_dir, test_flag=self.test)
+        #centroid_evaluation(self.model, self.test_loader, self.device, self.out_dir)
+
+        # loading json data
+        pr_json = self.out_dir + "/precision_recall_results.json"
+        cen_err_json = self.out_dir + "/centroid_error.json"
         with open(pr_json) as pr_file:
             pr_dict = json.load(pr_file)
             pr_file.close()
         with open(cen_err_json) as ce_file:
             ce_dict = json.load(ce_file)
             ce_file.close()
-        
+
         # plotting data
-        plot_precision_recall(pr_dict['segm'], config_dict['plot_title'], config_dict['out_dir'])
-        plot_cent_err(ce_dict, config_dict['plot_title'], config_dict['out_dir'])
-        plot_f1_score(pr_dict['segm'], config_dict['plot_title'], config_dict['out_dir'])
-        
+        plot_precision_recall(pr_dict['segm'], self.plot_title, self.out_dir)
+        #plot_cent_err(ce_dict, self.plot_title, self.out_dir)
+        #plot_f1_score(pr_dict['segm'], self.plot_title, self.out_dir)
+
         # fps_value
-        fps = fps_evaluate(model, config_dict['im_test_path'], device)
+        fps = fps_evaluate(self.model, self.test_path, self.device)
         print(fps)
         
         # segmentation generation
-        segment_instance(device, config_dict['im_test_path'], ['__background__', 'jersey_royal'], model, 
-                         config_dict['plot_title'], config_dict['out_dir'])
-        centroid_instance(device, config_dict['im_test_path'], test_loader, model, config_dict['plot_title'], config_dict['out_dir'], thresh=0.95)
-        
+        segment_instance(self.device, self.test_path, ['__background__', 'jersey_royal'], self.model, 
+                         self.plot_title, self.out_dir)
+        centroid_instance(self.device, self.test_path, self.test_loader, self.model, self.plot_title,
+                         self.out_dir, thresh=0.95)
+
 # =================================================================================================
 # Train_net execution
 # =================================================================================================
@@ -283,12 +255,12 @@ if __name__ == "__main__":
     #            WORKERS=4 , MIN_MAX=[800, 1333], LR=0.005, NUM_EPOCHS=20,
     #            TEST_IM_STR="data/jersey_royal_dataset/test/169.JPG"):
     ###############################################################################################
-    TRAIN = True
-    TEST = False
-    LOAD = False
+    TRAIN = False
+    TEST = True
+    LOAD = True
     BEST = True
     
-    EPOCHS = 50
+    EPOCHS = 5
     BATCH_SIZE = 1
     WORKERS = 0
     ###############################################################################################
@@ -312,6 +284,6 @@ if __name__ == "__main__":
         # loop to train models through experiment
         for conf in conf_list:
             # calling main    
-            main(conf)
+            TrainNetwork(conf)
         
         idx += 1

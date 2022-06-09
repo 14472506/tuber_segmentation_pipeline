728bbb5ccaf5d7af62a58c04151f062abf25ac60
Your Name <you@example.com>
2022-06-09 23:17:32 +0100

    configuring

--- main.py
+++ main.py
@@ -15,27 +15,57 @@ def main():
     BEST = True
     
     # othere params
-    EPOCHS = 10
-    BATCH_SIZE = 1
+    EPOCHS = 150
+    BATCH_SIZE = 2
     WORKERS = 0
     LR_SCHEDULER = "lr_step"
-    SCH_PARAMS = [5, 0.001]
+    SCH_PARAMS = [50, 0.1]
 
     # loops params
-    idx = 3
-    lr_list = [0.005]
+    idx = 0
+    lr_list = [0.0005, 0.001, 0.005]
 
     for i in lr_list: 
         # setting up list of models
-        conf_list = [configs.conf_maker(TRAIN, TEST, "Mask_RCNN_R50_FPN", "test_"+str(idx), BATCH_SIZE=BATCH_SIZE,
-                                        WORKERS=WORKERS, LR=i, NUM_EPOCHS=EPOCHS, LOAD_FLAG=LOAD, LOAD_BEST=BEST, 
-                                        TRANSFORMS="combine_transforms", LR_SCHEDULER=LR_SCHEDULER, SCHEDULER_PARAMS=SCH_PARAMS)#,
-                    #configs.conf_maker(TRAIN, TEST, "Mask_RCNN_R50_FPN", "Colour_T_"+str(idx), BATCH_SIZE=1,
-                    #                    WORKERS=WORKERS, LR=i, NUM_EPOCHS=EPOCHS, LOAD_FLAG=LOAD, LOAD_BEST=BEST, 
-                    #                    TRANSFORMS="colour_transforms"),
-                    #configs.conf_maker(TRAIN, TEST, "Mask_RCNN_R50_FPN", "Combine_T_"+str(idx), BATCH_SIZE=1,
-                    #                    WORKERS=WORKERS, LR=i, NUM_EPOCHS=EPOCHS, LOAD_FLAG=LOAD, LOAD_BEST=BEST, 
-                    #                    TRANSFORMS="combine_transforms")
+        conf_list = [configs.conf_maker(TRAIN,
+                                        TEST,
+                                        "Mask_RCNN_R50_FPN",
+                                        "R50_S50-01_"+str(i),
+                                        BATCH_SIZE=BATCH_SIZE,
+                                        WORKERS=WORKERS,
+                                        LR=i, 
+                                        NUM_EPOCHS=EPOCHS,
+                                        LOAD_FLAG=LOAD, 
+                                        LOAD_BEST=BEST, 
+                                        TRANSFORMS="combine_transforms",
+                                        LR_SCHEDULER=LR_SCHEDULER,
+                                        SCHEDULER_PARAMS=SCH_PARAMS),
+                     configs.conf_maker(TRAIN,
+                                        TEST,
+                                        "Mask_RCNN_R18_FPN",
+                                        "R18_S50-01_"+str(i),
+                                        BATCH_SIZE=BATCH_SIZE,
+                                        WORKERS=WORKERS,
+                                        LR=i, 
+                                        NUM_EPOCHS=EPOCHS,
+                                        LOAD_FLAG=LOAD, 
+                                        LOAD_BEST=BEST, 
+                                        TRANSFORMS="combine_transforms",
+                                        LR_SCHEDULER=LR_SCHEDULER,
+                                        SCHEDULER_PARAMS=SCH_PARAMS),
+                     configs.conf_maker(TRAIN,
+                                        TEST,
+                                        "Mask_RCNN_R34_FPN",
+                                        "R34_S50-01_"+str(i),
+                                        BATCH_SIZE=BATCH_SIZE,
+                                        WORKERS=WORKERS,
+                                        LR=i, 
+                                        NUM_EPOCHS=EPOCHS,
+                                        LOAD_FLAG=LOAD, 
+                                        LOAD_BEST=BEST, 
+                                        TRANSFORMS="combine_transforms",
+                                        LR_SCHEDULER=LR_SCHEDULER,
+                                        SCHEDULER_PARAMS=SCH_PARAMS)
                     ]
 
         # loop to train models through experiment

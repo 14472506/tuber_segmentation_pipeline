7dd79dd5d04df0f53f60095aa4adcef610c5190d
Your Name <you@example.com>
2022-06-09 19:23:18 +0100

    refactoring main

--- main.py
+++ main.py
@@ -1,36 +1,31 @@
 """
 Details
 """
+# imports
 from loop.train_net import TrainNetwork
 import config.configs as configs
 
-if __name__ == "__main__":
-    
-    # conf dict list for experimental setup
-    # Default setting as seen below:
-    # ==============================
-    # conf_maker(TRAIN, TEST, MODEL, OUT_DIR, TRANSFORMS="", LOAD_FLAG=False, LOAD_BEST=True, BATCH_SIZE=2,
-    #            WORKERS=4 , MIN_MAX=[800, 1333], LR=0.005, NUM_EPOCHS=20,
-    #            TEST_IM_STR="data/jersey_royal_dataset/test/169.JPG"):
-    ###############################################################################################
+# main function
+def main():
+
+    # Boolean flags
     TRAIN = True
     TEST = False
     LOAD = False
     BEST = True
     
+    # othere params
     EPOCHS = 10
     BATCH_SIZE = 1
     WORKERS = 0
     LR_SCHEDULER = "lr_step"
-    ###############################################################################################
-    
+
+    # loops params
     idx = 1
     lr_list = [0.001]
-    
-    for i in lr_list:
-        
-        # setting up list of models
 
+    for i in lr_list: 
+        # setting up list of models
         conf_list = [configs.conf_maker(TRAIN, TEST, "Mask_RCNN_R50_FPN", "test_"+str(idx), BATCH_SIZE=1,
                                         WORKERS=WORKERS, LR=i, NUM_EPOCHS=EPOCHS, LOAD_FLAG=LOAD, LOAD_BEST=BEST, 
                                         TRANSFORMS="combine_transforms")#,
@@ -48,3 +43,7 @@ if __name__ == "__main__":
             TrainNetwork(conf)
         
         idx += 1
+
+# execution
+if __name__ == "__main__":
+    main()
